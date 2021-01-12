d660964b80270ecc170be3197db99c9f3b589e6c
anuj-algoworks <anuj.kumar@algoworks.com>
2021-01-12 09:42:43 +0530

    initial commit

---
+++ src/strings/CheckAlphabets.java
@@ -0,0 +1,26 @@
+package strings;
+
+/**
+ * 
+ * @author anuj
+ * Check if a string contains only alphabets in Java using Lambda expression
+ * Examples:
+
+	Input : GeeksforGeeks
+	Output : True
+
+	Input : Geeks4Geeks
+	Output : False
+
+	Input : null
+	Output : False
+ *
+ */
+public class CheckAlphabets {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
