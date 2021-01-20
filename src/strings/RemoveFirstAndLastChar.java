02e0a4b3eb661f9657d94590f777ebdaf5c969e9
kanujgit <anujkumar30dec@gmail.com>
2021-01-20 12:28:00 +0530

    Added strings related codes

---
+++ src/strings/RemoveFirstAndLastChar.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class RemoveFirstAndLastChar {
+
+	public static void main(String[] args) {
+		String str="ANuj";
+		StringBuilder builder = new StringBuilder(str);
+		builder.deleteCharAt(0);
+		builder.deleteCharAt(builder.length()-1);
+		System.out.println(builder.toString());
+
+	}
+
+}
